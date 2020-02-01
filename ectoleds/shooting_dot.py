d8da8721f16f8b7d966433f6997f1f0628d93060
Neil <neilrodm@gmail.com>
2020-02-01 13:32:59 -0600

    added upload

--- ectoleds/shooting_dot.py
+++ ectoleds/shooting_dot.py
@@ -1,9 +1,6 @@
 from ectoleds.effect_base import Effect
 import adafruit_dotstar as dotstar
-from ectoleds.color_utils import mergeColor
 import time
-import math
-from enum import Enum
 
 
 class Shooting(Effect):
@@ -14,21 +11,18 @@ class Shooting(Effect):
         self.index = 0
 
     def apply(self, leds: dotstar.DotStar, respectLedsState=False):
-        if time.time()-self.previousTime >= .25:
+        if time.time() - self.previousTime >= .25:
             print(self.index)
             self.previousTime = time.time()
             leds[self.index] = self.shootingColor
 
             if self.index != 0:
-                leds[self.index-1] = 0
+                leds[self.index - 1] = 0
 
             if self.index == 0:
-                leds[self.ledAmount-1] = 0
+                leds[self.ledAmount - 1] = 0
 
-            if self.index >= self.ledAmount-1:
+            if self.index >= self.ledAmount - 1:
                 self.index = 0
             else:
                 self.index = self.index + 1
-
-
-
--- leds.py
+++ leds.py
@@ -4,6 +4,7 @@ import adafruit_dotstar as dotstar
 from ectoleds import breathe
 from ectoleds import random_blink
 from ectoleds import shooting_dot
+from ectoleds import  shooting_Star_Reverse
 
 num_leds = 13
 dots = dotstar.DotStar(board.SCK, board.MOSI, num_leds, brightness=0.5, auto_write=False)
@@ -11,11 +12,13 @@ dots = dotstar.DotStar(board.SCK, board.MOSI, num_leds, brightness=0.5, auto_wri
 breatheEffect = breathe.Breathe(ledAmount=num_leds, breatheColor=(255, 0, 0))
 randomBlinkEffect = random_blink.RandomBlink(ledAmount=num_leds, blinkColor=(255, 0, 0))
 shootingdotEffect = shooting_dot.Shooting(ledAmount=num_leds, shootingColor=(0, 0, 50))
+shootingDotReverseEffect = shooting_Star_Reverse.ShootingReverse(ledAmount=num_leds, ledColor=(0, 0, 255))
 
 while True:
     # breatheEffect.apply(dots)
     # randomBlinkEffect.apply(dots, respectLedsState=True)
-    shootingdotEffect.apply(dots)
+    #shootingdotEffect.apply(dots)
+    shootingDotReverseEffect.apply(dots)
 
     dots.show()
 
